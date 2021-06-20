2fc4c35069b4dd1f367674918b2281f85f42ea88
Olusegun Faith Emmanuel <faitholusegun60@gmail.com>
2021-06-20 11:24:39 +0100

    Update UnitTest1.cs

--- Facemask/Facemask.Test/UnitTest1.cs
+++ Facemask/Facemask.Test/UnitTest1.cs
@@ -8,14 +8,14 @@ namespace Facemask.Test
         [Theory]
         [InlineData(2)]
         [InlineData(4)]
-        [InlineData(5)]
+        [InlineData(50)]
         [InlineData(16)]
         [InlineData(8)]
         [InlineData(6)]
         [InlineData(10)]
         [InlineData(12)]
-        [InlineData(3)]
-        [InlineData(9)]
+        [InlineData(32)]
+        [InlineData(18)]
         public void Test1(int num)
         {
             bool isEven = true;

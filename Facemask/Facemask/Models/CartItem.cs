129862b6d4d9a58de75c2a8c595a52cd2af9abb2
Olusegun Faith Emmanuel <faitholusegun60@gmail.com>
2021-06-20 11:38:15 +0100

    Create CartItem.cs

---
+++ Facemask/Facemask/Models/CartItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facemask.Models
+{
+    public class CartItem
+    {
+        public Product? Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
